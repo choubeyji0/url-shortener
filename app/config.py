21136038b12fab895d6b2ceb336f25a23c6051f9
choubeyji0 <Abhishekchoubey312@gmail.com>
2026-01-02 21:42:50 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,8 @@
+import os
 
+BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://user:password@localhost:5432/url_shortener"
+)
