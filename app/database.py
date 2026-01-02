5ebfa0cb0a52337474f831a269f38ff4ef1b0f95
choubeyji0 <Abhishekchoubey312@gmail.com>
2026-01-02 21:32:11 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+DATABASE_URL = "postgresql://user:password@localhost:5432/url_shortener"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
