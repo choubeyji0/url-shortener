7698a5992914c6adc99b63644196a3ab47160f5a
choubeyji0 <Abhishekchoubey312@gmail.com>
2026-01-02 21:34:38 +0530

    Update shorten.py

--- app/routes/shorten.py
+++ app/routes/shorten.py
@@ -1,11 +1,25 @@
 from fastapi import APIRouter
 from app.schemas import ShortenURLRequest, ShortenURLResponse
 from app.services.short_code import generate_short_code
+from app.database import SessionLocal
+from app.models import URL
 
 router = APIRouter(prefix="/api/v1")
 
 @router.post("/shorten", response_model=ShortenURLResponse)
 def shorten_url(request: ShortenURLRequest):
+    db = SessionLocal()
+
     short_code = generate_short_code()
+
+    url = URL(
+        long_url=request.long_url,
+        short_code=short_code
+    )
+
+    db.add(url)
+    db.commit()
+    db.close()
+
     short_url = f"http://localhost:8000/{short_code}"
     return ShortenURLResponse(short_url=short_url)
