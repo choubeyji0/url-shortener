9ca361e2dfb152582f41341f5c0f01405704b400
choubeyji0 <Abhishekchoubey312@gmail.com>
2026-01-02 21:37:50 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
 from app.routes.shorten import router as shorten_router
+from app.database import Base, engine
+from app.models import URL
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(shorten_router)
 
 @app.get("/")
 def home():
     return {"message": "URL Shortener is running"}
-
-from app.database import Base, engine
-from app.models import URL
-
-Base.metadata.create_all(bind=engine)
